30f21ed36ad34c824a268ffa7fd9b81a5861c0b6
Re <elbandito237@gmail.com>
2024-02-22 21:52:19 +0600

    Нужно разобраться с выбором в начале и декриптом, вручную все нормально работает

---
+++ Stepik/"Поколение Python": курс для начинающих/15/2/Шифр Цезаря.py
@@ -0,0 +1,21 @@
+# https://stepik.org/lesson/352860/step/4?unit=336821
+
+direction = ('encryption', 'decryption')
+language = {'eng': [122, 26], 'rus': [1103, 32]}
+input_rotate = 10
+input_text = 'Блажен, кто верует, тепло ему на свете!'.lower()
+
+
+def crypt(text, max_symbol, max_letter, rotate):
+    word = ''
+    for i in text:
+        if ord(i) + rotate > max_symbol:
+            word += chr((ord(i) - (max_letter - rotate)))
+        elif i in (' ', '?', '!', ',', '.'):
+            word += i
+        else:
+            word += chr(ord(i) + rotate)
+    return word.capitalize()
+
+
+print(crypt(input_text, language['rus'][0], language['rus'][1], input_rotate))
