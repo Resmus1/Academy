67e55c4de1e5bce83c1453ef636f231d4eed5d0c
Re <elbandito237@gmail.com>
2024-02-23 07:48:10 +0600

    Перенес в словарь, нужно наладить настройку словаря или перенести генерацию смловаря в функцию

--- Stepik/"Поколение Python": курс для начинающих/15/2/Шифр Цезаря.py
+++ Stepik/"Поколение Python": курс для начинающих/15/2/Шифр Цезаря.py
@@ -1,9 +1,15 @@
 # https://stepik.org/lesson/352860/step/4?unit=336821
 
-direction = ('encryption', 'decryption')
-language = {'eng': [122, 26], 'rus': [1103, 32]}
-input_rotate = 10
-input_text = 'Блажен, кто верует, тепло ему на свете!'.lower()
+# direction = ('encryption', 'decryption')
+# language = {'eng': [122, 26], 'rus': [1103, 32]}
+# input_rotate = 10
+# input_text = 'Блажен, кто верует, тепло ему на свете!'.lower()
+setting = {'direction': 1, 'language': {'eng': [122, 26], 'rus': [1103, 32]}, 'input_rotate': 0, 'input_text': ''}
+
+def setting_encrypt():
+    """
+    Записать в функцию параметры из настроек, совместить по параметрам с генератором паролей
+    """
 
 
 def crypt(text, max_symbol, max_letter, rotate):
