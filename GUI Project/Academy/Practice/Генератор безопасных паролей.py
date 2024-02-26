00b5ee16f88f86654adeff88124a671abce16935
Re <elbandito237@gmail.com>
2024-02-26 11:27:44 +0600

    Начал работу над GUI генератора, добавил чекбоксы, текст и иконку, отредактировал вид

---
+++ GUI Project/Academy/Practice/Генератор безопасных паролей.py
@@ -0,0 +1,52 @@
+import tkinter as tk
+
+win = tk.Tk()
+win.geometry(f"600x300+100+200")
+win.title("Генератор безопасных паролей")
+icon = tk.PhotoImage(file='/home/deck/pythonProject/pythonProject/Academy/GUI Project/Academy/img/password_icon.png')
+win.iconphoto(False, icon)
+win.config(bg='#CD853F')
+win.resizable(False, False)
+
+label = tk.Label(text="Генерируем пароль по следующим параметрам:",
+                 bg='#CD853F',
+                 fg='#B71C1C',
+                 font=("Helvetica", 15, "bold"),
+                 relief=tk.RIDGE,
+                 bd=10,
+                 )
+
+num_checkbutton = tk.Checkbutton(text="Включать ли цифры?",
+                                 bg='#CD853F',
+                                 activebackground='#CD853F',
+                                 highlightthickness=0,
+                                 )
+up_checkbutton = tk.Checkbutton(text="Включать ли прописные буквы?",
+                                bg='#CD853F',
+                                activebackground='#CD853F',
+                                highlightthickness=0,
+                                )
+low_checkbutton = tk.Checkbutton(text="Включать ли строчные буквы?",
+                                 bg='#CD853F',
+                                 activebackground='#CD853F',
+                                 highlightthickness=0,
+                                 )
+symbol_checkbutton = tk.Checkbutton(text="Включать ли символы?",
+                                    bg='#CD853F',
+                                    activebackground='#CD853F',
+                                    highlightthickness=0,
+                                    )
+similar_checkbutton = tk.Checkbutton(text="Исключать ли неоднозначные символы?",
+                                     bg='#CD853F',
+                                     activebackground='#CD853F',
+                                     highlightthickness=0,
+                                     )
+
+label.pack()
+num_checkbutton.pack()
+up_checkbutton.pack()
+low_checkbutton.pack()
+symbol_checkbutton.pack()
+similar_checkbutton.pack()
+
+win.mainloop()
