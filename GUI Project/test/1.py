ecf518c279025987a58e6fcce19e363288d0cc90
Re <elbandito237@gmail.com>
2024-02-26 10:12:30 +0600

    end

---
+++ GUI Project/test/1.py
@@ -0,0 +1,16 @@
+# Введение в tkinter. Главное окно
+
+import tkinter as tk
+
+win = tk.Tk()
+h = 500
+w = 600
+icon = tk.PhotoImage(file='img/test.png')
+win.iconphoto(False, icon)
+win.config(bg='#CD853F')
+win.title('Тестируем Ткинтер')
+win.geometry(f"{h}x{w}+100+200")
+win.minsize(300, 400)
+win.maxsize(700, 800)
+win.resizable(True, True)
+win.mainloop()
