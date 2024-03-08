3eb6abea9ead20a69dfcbd55e01549eb1cf60247
Re <elbandito237@gmail.com>
2024-03-08 19:40:53 +0600

    upd

--- Stepik/"Поколение Python": курс для продвинутых/2/8.py
+++ Stepik/"Поколение Python": курс для продвинутых/2/8.py
@@ -1,7 +1,9 @@
 # https://stepik.org/lesson/415554/step/8?unit=405083
 
 options = ["камень", "ящерица", "Спок", "ножницы", "бумага"]
-score = options.index(input()) - options.index(input())
+
+timur, ruslan = options.index(input()), options.index(input())
+score = timur - ruslan
 if score in (2, 4, -1, -3):
     print('Тимур')
 elif score in (1, 3, -2, -4):
