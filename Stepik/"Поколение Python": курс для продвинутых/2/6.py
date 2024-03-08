645658c6103cceb68744f9c4e2e70f20cec87094
Re <elbandito237@gmail.com>
2024-03-08 17:35:47 +0600

    Finish

--- Stepik/"Поколение Python": курс для продвинутых/2/6.py
+++ Stepik/"Поколение Python": курс для продвинутых/2/6.py
@@ -1,4 +1,16 @@
 # https://stepik.org/lesson/415554/step/6?unit=405083
 
-l = input()
-print(l)
+size = int(input())
+seq = [int(input()) for _ in range(size)]
+number = int(input())
+flag = False
+
+for i in range(size):
+    for j in range(size):
+        if i != j and seq[i] * seq[j] == number:
+            flag = True
+
+if flag:
+    print("ДА")
+else:
+    print("НЕТ")
